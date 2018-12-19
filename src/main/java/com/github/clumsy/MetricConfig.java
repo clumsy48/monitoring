ca03ad92fe72b646f3b104dfc7815f9e358b8648
Krrishna Murari <m007urray@gmail.com>
2018-12-19 18:20:19 +0530

    counter

---
+++ src/main/java/com/github/clumsy/MetricConfig.java
@@ -0,0 +1,26 @@
+package com.github.clumsy;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Slf4j
+@Configuration
+@Profile({"test"})
+@RequiredArgsConstructor
+public class MetricConfig {
+
+    private final MeterRegistry meterRegistry;
+
+    @Bean
+    public Counter getSimpleCounter(){
+        return Counter.builder("instance")
+                .tag("dev", "performance")
+                .description("simple counter")
+                .register(meterRegistry);
+    }
+}
